19baf8f07aed20d770bcd51831fa3a769d6cf4ca
Lucas Yang <yangchenshin77@gmail.com>
2026-02-19 02:56:05 +0800

    Release v0.2.1

--- src/McpService.ts
+++ src/McpService.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 import { ISpecExplorer } from "./core/interfaces/ISpecService";
 import { ConsoleLogger, Logger } from "./core/Logger";
 
-export const VERSION = "0.2.0";
+export const VERSION = "0.2.1";
 
 export class McpService {
   private readonly logger: Logger;
